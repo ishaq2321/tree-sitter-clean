260af40f7e2e0741bfefc0e69033a23a1bc00230
Muhammad Ishaq Khan <ishaq2321@users.noreply.github.com>
2026-06-11 21:38:44 +0200

    chore: fix CI, highlights, extensions, add test coverage
    - CI: switch to official tree-sitter/workflows reusable action, fix branch master
    - Highlights: add keyword captures (module, import, class, let, case, etc.),
      fix class/instance name from @keyword to @type, add operator/punctuation captures
    - Remove ambiguous .abc file extension (Clean uses .icl/.dcl only)
      implementation/definition/system modules
    - 32/32 tests passing

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,15 +1,54 @@
+; Keywords
+"module" @keyword
+"import" @keyword
+"from" @keyword
+"where" @keyword
+"with" @keyword
+"class" @keyword
+"instance" @keyword
+"let" @keyword
+"in" @keyword
+"case" @keyword
+"of" @keyword
+"implementation" @keyword
+"definition" @keyword
+"system" @keyword
+
+; Module and import names
 (module_declaration (identifier) @module)
 (import_declaration (identifier) @module)
+
+; Types and constructors
 (type_signature (identifier) @type)
 (type_definition (identifier) @type)
-(function_declaration (identifier) @function)
-(class_declaration (identifier) @keyword)
-(instance_declaration (identifier) @keyword)
-(string) @string
-(number) @number
-(line_comment) @comment
-(block_comment) @comment
+(class_declaration (identifier) @type)
+(instance_declaration (identifier) @type)
 ((identifier) @constructor
  (#match? @constructor "^[A-Z]"))
+
+; Functions and variables
+(function_declaration (identifier) @function)
 ((identifier) @variable
  (#not-match? @variable "^[A-Z]"))
+
+; Literals
+(string) @string
+(number) @number
+
+; Comments
+(line_comment) @comment
+(block_comment) @comment
+
+; Operators
+(operator) @operator
+
+; Patterns
+(wildcard) @keyword
+
+; Punctuation
+"=" @operator
+"->" @operator
+"," @punctuation.delimiter
+"." @punctuation.delimiter
+"|" @operator
+"::" @operator
